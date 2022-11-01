dd33f860fc41228ed14f65cfccc545ac415aec0a
Olga Stepanova <bolshakova.olmh@gmail.com>
2022-11-01 23:25:13 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,79 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException, SAXException {
+        createCSVFile();
+
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json, "data.json");
+    }
+
+    private static void writeString(String name, String fileName) {
+        try (FileWriter file = new
+                FileWriter(fileName)) {
+            file.write(name);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> list = null;
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy =
+                    new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            list = csv.parse();
+            list.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void createCSVFile() {
+        String[] employee1 = "1,John,Smith,USA,25".split(",");
+        String[] employee2 = "2,Inav,Petrov,RU,23".split(",");
+
+        try (CSVWriter writer = new CSVWriter(new FileWriter("data.csv"))) {
+            writer.writeNext(employee1);
+            writer.writeNext(employee2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
